adc2b325356fb49af5f76177ee825ced29caba2e
Matheus <matheus.encode@gmail.com>
2021-05-15 19:09:31 -0300

    Descoberto um bug de irmão nulo

--- ArvoreRedBlack.java
+++ ArvoreRedBlack.java
@@ -7,10 +7,14 @@ public class ArvoreRedBlack extends ArvoreAbstract {
 
     @Override
     public void inserir(int valor) {
+
         this.inserir(this.raiz, valor);
+
         System.out.println("Inserindo o valor: " + valor);
-        
-        
+
+        if(precisaValidar(this.raiz)) {
+            System.out.println("Precisa Balancear!!!");
+        }
     }
 
     @Override
@@ -23,4 +27,82 @@ public class ArvoreRedBlack extends ArvoreAbstract {
         return this.altura(this.raiz);
     }
 
+    private boolean precisaValidar(No no) {
+
+        if(no.getDireito() != null) {
+            if(precisaValidar(no.getDireito())) {
+                return true;
+            }
+        }
+        
+        if(no.getEsquerdo() != null) {
+            if(precisaValidar(no.getEsquerdo())) {
+                return true;
+            }
+        }
+        
+        if(no.isRedNode()) {
+
+            if(no.getDireito() != null) {
+                if(no.getDireito().isRedNode()) {
+                    balancear(no, "Direita");
+                }
+            }
+
+            if(no.getEsquerdo() != null) {
+                if(no.getEsquerdo().isRedNode()) {
+                    balancear(no, "Esquerda");
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void balancear(No no, String ladoFilho) {
+        if(no.getPai().getEsquerdo() == no) {
+            if(no.getPai().getDireito() == null) {
+                
+            }
+            if(no.getPai().getDireito().isRedNode()) {
+
+                no.getPai().getDireito().setBlackNode();
+                no.setBlackNode();
+
+                if(!no.getPai().isRoot()) {
+                    no.getPai().setRedNode();
+                }
+            }
+
+            else {
+                if(ladoFilho == "Direita") {
+
+                }
+
+                else {
+                    no.getPai().setRedNode();
+                    no.setBlackNode();
+
+                    // No noAux = no.getPai();
+                    // no.setPai(no.getPai().getPai());
+                    // no.getPai().setPai(no);
+                    // no.setEsquerdo(no);
+
+                }
+            }
+        }
+
+        else {
+
+            if(no.getPai().getEsquerdo().isRedNode()) {
+                
+                no.getPai().getEsquerdo().setBlackNode();
+                no.setBlackNode();
+
+                if(!no.getPai().isRoot()) {
+                    no.getPai().setRedNode();
+                }
+            }
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -2,11 +2,8 @@ public class Main {
 
     public static void main(String [] args) {
 
-        ArvoreRedBlack arb = new ArvoreRedBlack(5);
-        arb.inserir(2);
-        arb.inserir(4);
-        arb.inserir(8);
-        arb.inserir(6);
+        ArvoreRedBlack arb = new ArvoreRedBlack(10);
+        arb.inserir(14);
+        arb.inserir(11);
     }
-    
 }
