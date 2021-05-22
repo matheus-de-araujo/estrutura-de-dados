dfd452ef0539af337472ed71dff7246fb2e48a65
Matheus <matheus.encode@gmail.com>
2021-05-22 13:26:22 -0300

    Deu tudo certo

--- Main.java
+++ Main.java
@@ -2,14 +2,16 @@ public class Main {
 
     public static void main(String [] args) {
 
-        ArvoreRedBlack arb = new ArvoreRedBlack(1);
+        ArvoreRedBlack arb = new ArvoreRedBlack(8);
         arb.inserir(2);
+        arb.inserir(1);
         arb.inserir(3);
         arb.inserir(4);
-        arb.inserir(5);
-        arb.inserir(6);
         arb.inserir(7);
-        arb.inserir(8);
+        arb.inserir(5);
         arb.inserir(9);
+
+        System.out.println("Acabou");
+
     }
 }
